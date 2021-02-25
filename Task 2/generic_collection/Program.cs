c459ec84748de18de1c1c2c723503ff07101740f
OlehKhlebinskyiIv <khlebinskyi@gmail.com>
2021-02-25 23:17:50 +0200

    Task2. Generic.

---
+++ Task 2/generic_collection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My List\n");
+
+            MyList<int> MyFirstList = new MyList<int>();
+
+            MyFirstList.Add(4);
+            MyFirstList.Add(5);
+            MyFirstList.Add(6);
+            MyFirstList.Add(7);
+            MyFirstList.Add(8);
+
+            Console.WriteLine($"Count = {MyFirstList.Count()}");
+            foreach (var item in MyFirstList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine($"Contains = {MyFirstList.Contains(5)}");
+            MyFirstList.Clear();
+            Console.WriteLine("After Clear");
+            Console.WriteLine($"Count = {MyFirstList.Count()}");
+
+            foreach (var item in MyFirstList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("End");
+        }
+    }
+}
