2d31780ead75c7af0aa9ed23939a53b2b0655554
OlehKhlebinskyiIv <khlebinskyi@gmail.com>
2021-03-08 00:05:07 +0200

    Task 3. Delegate.

---
+++ Task 3/Delegare/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Delegare
+{
+
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+
+        //
+
+            AlphaNumericCollector alphaNumericCollector = new AlphaNumericCollector();
+            StringCollector stringCollector = new StringCollector();
+            WriteLine writeLine = new WriteLine();
+
+            writeLine.collectorDelegate += stringCollector.Collector;
+            writeLine.collectorDelegate += alphaNumericCollector.Collector;
+
+            writeLine.WriteLineFunc();
+
+            Console.ReadKey();
+        }
+
+    }
+}
